20c7579025bf206ef40a838143b93e4044258080
pRaNaY <www.pranay1337.com@gmail.com>
2023-07-25 18:22:26 +0530

    completed app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'price_screen.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
